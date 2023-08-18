0a5547296d4da3832c29bfe8fbd6ffba09452527
Your Name <you@example.com>
2023-08-18 01:11:54 -0400

    new try

--- exercises.c
+++ exercises.c
@@ -187,7 +187,7 @@ void inicializarLibro(Libro *libro, const char *titulo, const char *nombreAutor,
   strcpy(libro -> titulo, titulo);
   strcpy(libro -> autor.nombre, nombreAutor);
   libro -> autor.anioNacimiento = anioNacimiento;
-  
+  libro -> anioPublicacion = anioPublicacion;
 }
 
 /*
